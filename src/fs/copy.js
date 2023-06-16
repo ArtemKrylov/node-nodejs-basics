24b574a50f7f7a6b74ca5909f40da0f8d0ef0ae6
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-16 11:25:20 +0300

    solved copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { access, copyFile, mkdir, readdir } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const filesPath = "./src/fs/files";
+  const filesCopyPath = "./src/fs/files_copy";
+
+  try {
+    await access(filesPath);
+    await mkdir(filesCopyPath, () => {});
+    const files = await readdir(filesPath);
+    files.forEach(async (file) => {
+      await copyFile(`${filesPath}/${file}`, `${filesCopyPath}/${file}`);
+    });
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
 await copy();
