72f750de404fd59f0155ee9358770d3b2132f6a5
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-16 11:36:55 +0300

    solved rename.js and delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import { unlink } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await unlink("./src/fs/files/fileToRemove.txt");
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import * as fs from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.rename(
+      "./src/fs/files/wrongFileName.txt",
+      "./src/fs/files/properFilename.md"
+    );
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
 await rename();
