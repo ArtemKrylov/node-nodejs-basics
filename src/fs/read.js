38be69368c717d7d2e809c54067a9aa3bc3b0cde
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-16 11:47:53 +0300

    solved list.js and read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const fileContent = await readFile(
+      "./src/fs/files/fileToRead.txt",
+      "utf-8"
+    );
+    console.log("fileContent of ./src/fs/files/fileToRead.txt:\n", fileContent);
+  } catch (error) {
+    throw new Error(`FS operation failed: ${error.message}`);
+  }
 };
 
 await read();
