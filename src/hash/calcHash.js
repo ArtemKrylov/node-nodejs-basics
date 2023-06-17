a95065b4ed63208bac954084549db12f51e420b0
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-17 22:29:44 +0300

    solved calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,12 +1,17 @@
 // calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex
 import { readFile } from 'node:fs/promises';
+import crypto from 'crypto';
 
 const calculateHash = async () => {
   // Write your code here
   try {
     const filePath = './src/hash/files/fileToCalculateHashFor.txt';
     const fileData = await readFile(filePath, 'utf-8');
-    console.log('fileData: ', fileData);
+
+    const hash = crypto.createHash('SHA256');
+    hash.update(fileData);
+    const hex = hash.digest('hex');
+    console.log('SHA256 hex: ', hex);
   } catch (error) {
     throw new Error(error.message);
   }
