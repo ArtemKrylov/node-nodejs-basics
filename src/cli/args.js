2de097b89293c46c5a3d3b33f6046ed1c3cc5b11
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-17 11:19:50 +0300

    solved Command line interface tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import * as util from "node:util";
+
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  // const comLineArgs = process.argv;
+  // console.log("command line arguments:\n ", comLineArgs);
+	const args = process.argv.slice(2);
+	const pattern = /^--/;
+	const argNames = args.filter(arg => pattern.test(arg));
+	const argValues = args.filter(arg => !pattern.test(arg));
+	console.log('command line arguments:\n ', argNames.map((el, i) => `${el} is ${argValues[i]}`).join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envVars = process.env;
+  console.log(
+    "environment variables:\n ",
+    Object.keys(envVars)
+      .map((key) => `RSS_${key}=${envVars[key]}`)
+      .join("; ")
+  );
 };
 
 parseEnv();
